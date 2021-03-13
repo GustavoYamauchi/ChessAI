821b6f08801fc8cf9660b9d99bd7dd1cf7ae891f
GustavoYamauchi <gyxgames31@gmail.com>
2021-03-13 02:01:32 -0300

    Jogando com a logica de minimax com evaluate

--- main.py
+++ main.py
@@ -19,20 +19,27 @@ pieces = {
 }
 
 
-def evaluate():
+def evaluate(board):
     
+    vWhite = 0
+    vBlack = 0
+    for piece in board.piece_map().values():
+        if piece.color == chess.BLACK:
+            vBlack += pieces[piece.piece_type]
+        else:
+            vWhite += pieces[piece.piece_type]
 
-
+    # print(f'White = {vWhite} - Black = {vBlack} \n')
     #StartGame -> Logica de Inicio
 
     #MidGame -> Preço das peças
         
     #FinalGame -> Algoritmos de Vitoria
-    return 0
+    return vWhite - vBlack
 
 def populateTree(fen, depth):
     if depth == 0:
-        return
+        return evaluate()
 
     legalMoves = list(board.legal_moves)
     tree[fen] = legalMoves
@@ -41,56 +48,73 @@ def populateTree(fen, depth):
         populateTree(board.fen(), depth - 1)
         board.pop()
 
+def minimax(board, depth, maximizing_player):
+    if depth == 0 or board.is_game_over():
+        return evaluate(board), board.peek()
+
+    if maximizing_player:
+        value = -float('inf')
+        lastMove = None
+        for move in board.legal_moves:
+            board.push(move)
+            valueAux = minimax(board, depth - 1, False)
+            if value > valueAux[0]:
+                board.pop()
+            else:
+                value = valueAux[0]
+                lastMove = board.pop()
+        return value, lastMove
+
+    else:
+        value = float('inf')
+        lastMove = None
+        for move in board.legal_moves:
+            board.push(move)
+            valueAux = minimax(board, depth - 1, True)
+            if value < valueAux[0]:
+                board.pop()
+            else:
+                value = valueAux[0]
+                lastMove = board.pop()
+        return value, lastMove
+
+
+def jogar():
+    while (not board.is_game_over()):
+        if(board.turn == chess.WHITE):
+            legalMoves = list(board.legal_moves)
+            board.push(minimax(board, 3, True)[1])
+            print("WHITE")
+        else:
+            legalMoves = list(board.legal_moves)
+            board.push(minimax(board, 3, True)[1])
+            print("BLACK")
+
+        print(f'{board} \n')
+
+    if board.is_stalemate():
+        return "Stalmate"
+    if board.is_checkmate():
+        return "Checkmate"
+    if board.is_insufficient_material():
+        return "Insufficiente Material"
+    if board.is_seventyfive_moves():
+        return "SeventyFive"
+    if board.is_fivefold_repetition():
+        return "FiveFold"
+
+
 board = chess.Board()
-populateTree(board.fen(), 3)
-print(tree)
-# def minimax(board, depth, maximizing_player):
-#     if depth == 0 or board.is_game_over():
-#         return evaluate(board)
-#     if maximizing_player:
-#         value = -float('inf')
-#         for move in board.legal_moves:
-#             board.push(move)
-#             value = max(value, minimax(board, depth - 1, False))
-#             board.pop()
-#         return value
-#     else:
-#         value = float('inf')
-#         for move in board.legal_moves:
-#             board.push(move)
-#             value = min(value, minimax(board, depth - 1, True))
-#             board.pop()
-#         return value
+board.push_san("e4")
+board.push_san("e5")
+board.push_san("Qh5")
+board.push_san("Nc6")
+print(f'\n\n Que comecem os jogos \n\n')
+jogada = jogar()
+print(f'F I N A L - {jogada}')
+print(f'{board} \n')
+
+# populateTree(board.fen(), 1)
+# print(tree)
 
 
-# def jogar():
-#     while (not board.is_game_over()):
-#         if(board.turn == chess.WHITE):
-#             legalMoves = list(board.legal_moves)
-#             board.push(random.choice(legalMoves))
-#         else:
-#             legalMoves = list(board.legal_moves)
-#             board.push(random.choice(legalMoves))
-
-#     if board.is_stalemate():
-#         return 0
-#     if board.is_checkmate():
-#         return 1
-#     if board.is_insufficient_material():
-#         return 2
-#     if board.is_seventyfive_moves():
-#         return 3
-#     if board.is_fivefold_repetition():
-#         return 4
-
-
-# board = chess.Board()
-# vitoria = [0, 0, 0, 0, 0]
-
-
-# for jogos in range(1000):
-#     print(jogos)
-#     board.reset()
-#     vitoria[jogar()] += 1
-
-# print(vitoria)
